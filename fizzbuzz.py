094539c9fe86d4daf3cad8dc1ea99f7c99704052
himchan <himchan9494@gmail.com>
2021-11-25 15:19:19 +0900

    feat: Print 'buzz' if i is times of 5
    TODO:
     - print fizzbuzz if i is times of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ def do_fizzbuzz():
     """
     Do fizzbuzz with for and if
     While i goes up 1 to 15,
-    print 'fizz' if i is times of 3
+    print 'fizz' if i is times of 3(done)
     print 'buzz' if i is times of 5
     print 'fizzbuzz' if i is times of 15
     else, print i (string)
@@ -10,6 +10,8 @@ def do_fizzbuzz():
     for i in range(1,15+1):
         if i%3==0:
             print('fizz')
+        elif i%5==0:
+            print('buzz')
         else:
             print('{}'.format(i))
     return True
