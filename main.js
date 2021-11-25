32f9bdecd2bd9e3fc90838f160d5d9be10c50f87
himchan <himchan9494@gmail.com>
2021-11-25 13:23:30 +0900

    feat: Creating FizzBuzz function
    Created FizzBuzz function

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// project 1
+
+let i = 1;
+
+while (i < 101) {
+  if (i % 3 === 0) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    } else {
+      console.log("fizz");
+    }
+  } else if (i % 5 === 0) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    } else {
+      console.log("buzz");
+    }
+  } else {
+    console.log(i);
+  }
+  i++;
+}
