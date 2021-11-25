258cbde159f16e8c60939c35dccde1158e273d01
himchan <himchan9494@gmail.com>
2021-11-25 15:28:03 +0900

    feat: fizzbuzz with if, for done

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,11 +4,13 @@ def do_fizzbuzz():
     While i goes up 1 to 15,
     print 'fizz' if i is times of 3(done)
     print 'buzz' if i is times of 5(done)
-    print 'fizzbuzz' if i is times of 15
+    print 'fizzbuzz' if i is times of 15(done)
     else, print i (string, done)
     """
     for i in range(1,15+1):
-        if i%3==0:
+        if i%15==0:
+            print('fizzbuzz')
+        elif i%3==0:
             print('fizz')
         elif i%5==0:
             print('buzz')
